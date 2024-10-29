abd079954b9250baadc5860007bb7e7840b76c69
Valentine Briese <valentinegb@icloud.com>
2024-10-29 16:22:35 -0700

    Implement test PAT

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,18 @@
+use std::env;
+
 use buy_me_a_coffee::MemberStatus;
+use dotenvy::dotenv;
+
+fn test_pat() -> String {
+    dotenv().ok();
+
+    env::var("TEST_PAT").expect("a personal access token must be provided to run tests")
+}
 
 #[tokio::test]
 #[should_panic = "401"]
 #[ignore = "unexpected behaviour from server"]
-async fn error() {
+async fn client_error() {
     let client = buy_me_a_coffee::Client::new("invalid access token");
 
     client.members(MemberStatus::All).await.unwrap();
